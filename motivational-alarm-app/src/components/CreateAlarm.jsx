7b91a188e063983fbaf24e490a4dd16311fdc955
Kareem Ezzat <Kareemezzat.125@gmail.com>
2022-12-25 23:45:47 +0200

    Styling wide screen fix

--- motivational-alarm-app/src/components/CreateAlarm.jsx
+++ motivational-alarm-app/src/components/CreateAlarm.jsx
@@ -3,15 +3,16 @@ import { StyleSheet, View } from "react-native";
 import TimePicker from "./TimePicker";
 import ListAlarms from "./ListAlarms";
 import { SafeAreaView, ImageBackground } from "react-native";
+
 export default function CreateAlarm() {
   return (
-    <View style={styles.container}>
+    <View style={{flex: 1}}>
       <ImageBackground
         source={require("../../assets/Images/background2.jpg")}
         style={{ flex: 1 }}
         resizeMode="cover"
       >
-        <View style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
+        <View style={styles.container}>
           <ListAlarms />
           <TimePicker />
         </View>
@@ -22,8 +23,43 @@ export default function CreateAlarm() {
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
-    alignItems: "center",
-    width:"100%"
-  },
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    backgroundColor: "rgba(0,0,0,0.4)",
+  }
 });
+
+
+
+// import React from "react";
+// import { StyleSheet, View } from "react-native";
+// import TimePicker from "./TimePicker";
+// import ListAlarms from "./ListAlarms";
+// import { SafeAreaView, ImageBackground } from "react-native";
+// export default function CreateAlarm() {
+//   return (
+//     <View style={styles.container}>
+//       <ImageBackground
+//         source={require("../../assets/Images/background2.jpg")}
+//         style={{ flex: 1 }}
+//         resizeMode="cover"
+//       >
+//         <View style={{ backgroundColor: "rgba(0,0,0,0.4)" }}>
+//           <ListAlarms />
+//           <TimePicker />
+//         </View>
+//       </ImageBackground>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     width:"100%"
+//   },
+// });
